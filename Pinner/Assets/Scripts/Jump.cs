845a99d702848b111a8d2d4cd305ff6c3bdc1ec3
Collin Gale <Collin.g086@gmail.com>
2020-10-05 15:46:13 -0600

    Jumpin and Runnin

---
+++ Pinner/Assets/Scripts/Jump.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+    public float moveSpeed = 7.5f;
+    public int jumpHeight = 3;
+    private int jumpCount = 1;
+
+    void Update()
+    {
+        /*//MOVEMENT
+        var horiAxis = Input.GetAxis("Horizontal");
+        var vertAxis = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horiAxis, 0,vertAxis);
+        controller.Move(move * (Time.deltaTime * moveSpeed));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 15f;
+        }
+        else
+        {
+            moveSpeed = 7.5f;
+        }
+        */
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            velocity.y = jumpHeight;
+            jumpCount++;
+        }
+        if (controller.isGrounded)
+        {
+            jumpCount = 1;
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
