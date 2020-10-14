412931f4912dd7389bcf334803c73ae7b1f2a61b
Collin Gale <Collin.g086@gmail.com>
2020-10-14 16:39:55 -0600

    Shooting and UI

---
+++ Pinner/Assets/Scripts/Shooting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour
+{
+    public int ammoCount;
+    private int maxAmmo = 20;
+    public GameObject prefab;
+    public Transform instancer;
+    public Slider ammoBar;
+    private WaitForSeconds reloadTime = new WaitForSeconds(2f);
+
+    private void Start()
+    {
+        ammoCount = maxAmmo;
+        ammoBar.maxValue = maxAmmo;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ammoCount > 0)
+        {
+            fire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(reload());
+        }
+
+        ammoBar.value = ammoCount;
+    }
+
+    private void fire()
+    {
+        Instantiate(prefab, instancer.position, instancer.rotation);
+        ammoCount--;
+        
+        if (ammoCount == 0)
+        {
+            //StartCoroutine(reload());
+        }
+    }
+
+    private IEnumerator reload()
+    {
+        yield return reloadTime;
+        ammoCount = maxAmmo;
+    }
+}
