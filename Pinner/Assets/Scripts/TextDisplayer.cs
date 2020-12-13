932a3d67a3e60a29c212cfc46b8796d81691581d
Collin Gale <Collin.g086@gmail.com>
2020-12-12 23:51:08 -0700

    NPC Windows and Minimap

---
+++ Pinner/Assets/Scripts/TextDisplayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplayer : MonoBehaviour
+{
+    public GameObject textPanel;
+    public Text textBox;
+    public string text;
+
+    private void Start()
+    {
+        textPanel.SetActive(false);
+        textBox.text = text;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        textPanel.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        textPanel.SetActive(false);
+    }
+}
